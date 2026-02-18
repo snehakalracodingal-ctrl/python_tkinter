6b8782a010736d63910fc63e7cfc98b005a15ce3
snehakalracodingal-ctrl <snehakalra.codingal@gmail.com>
2026-02-18 17:10:00 +0530

    first commit

---
+++ window.py
@@ -0,0 +1,12 @@
+# Import necessary libraries
+from tkinter import *
+
+# Create Window
+window = Tk()
+
+# Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('1000x500')
+
+# Start the GUI event loop
+window.mainloop()
